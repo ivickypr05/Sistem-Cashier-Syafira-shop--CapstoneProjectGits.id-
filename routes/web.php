865bfe492eebd63664285ef64cb5b9f9a1fed341
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-05-07 11:21:52 +0700

    perubahan alur login

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfilController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +16,8 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('profil');
 });
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/profil', [ProfilController::class, 'index'])->name('profil');
