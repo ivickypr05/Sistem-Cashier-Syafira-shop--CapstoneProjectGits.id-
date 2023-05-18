a434836947509f1d42feac91c2475b50078744b0
Siti Mutamimah <sitimutamimah669@gmail.com>
2023-05-18 20:17:40 +0700

    styling profile and login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="{{ asset('assets/css/admin.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+
     <link href="{{ asset('assets/js/main.js') }}" rel="stylesheet" />
 
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,50 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+
     <br><br><br>
     <style>
         body {
-            background-color: grey;
+            background-image: url('https://i.postimg.cc/mDMqF0wc/aaa.png');
+            background-size: cover;
+            background-image: no-repeat;
         }
     </style>
     <section class="py-2">
         <div class="container">
             <div class="row" id="profile">
                 <div class="col-lg-10 mx-auto">
-                    <blockquote class="blockquote blockquote-custom bg-white p-5 shadow rounded">
-                        <img src="https://w7.pngwing.com/pngs/522/396/png-transparent-computer-icons-profile-miscellaneous-logo-profile.png"
-                            style="width:200px">
-                        <div class="blockquote-custom-icon bg-warning shadow-sm"><i class="fa fa-quote-left text-white"></i>
-                        </div>
-                        <p class="mb-0 mt-2 font-italic">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo <a href="#"
-                                class="text-info">@consequat</a>."</p>
-                        <footer class="blockquote-footer pt-4 mt-4 border-top">Someone famous in
-                        </footer>
-                    </blockquote>
+                    <div class="col-lg-50">
+                        
+                    </div>
                 </div>
             </div>
-            <hr><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
             <div class="row" id="contact">
                 <div class="col-lg-50">
-                    <blockquote class="blockquote blockquote-custom bg-white p-5 shadow rounded">
-                        <img src="https://w7.pngwing.com/pngs/522/396/png-transparent-computer-icons-profile-miscellaneous-logo-profile.png"
-                            style="width:200px">
-                        <div class="blockquote-custom-icon bg-warning shadow-sm"><i class="fa fa-quote-left text-white"></i>
-                        </div>
-                        <p class="mb-0 mt-2 font-italic">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."</p>
-
+                    <blockquote class="blockquote blockquote-custom p-5 bg-light shadow rounded">
+                        <div class="blockquote-custom-icon bg-dark shadow-sm"><i class="fa fa-quote-left text-white"></i></div>
+                        <footer class="site-footer">
+                            <div class="container">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.0755268190364!2d108.5442965!3d-6.760645099999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f1de48193cd23%3A0x88cfbe5ae19ff6ea!2sToko%20Safira!5e0!3m2!1sen!2sid!4v1684412941665!5m2!1sen!2sid" 
+                                width="989" height="318" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        
                         </footer>
                     </blockquote>
                 </div>
             </div> <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i> </a>
         </div><br>
-        <div class="container">
+        <div class="container text-dark">
             <div class=" align-items-center small">
-                <div class="text-center">Copyright &copy; THORNY DEVIL 2023</div>
+                <div class="text-center">Copyright &copy; THORNY DEVIL 2023 - All Rights Reserved</div>
             </div>
         </div>
     @endsection
