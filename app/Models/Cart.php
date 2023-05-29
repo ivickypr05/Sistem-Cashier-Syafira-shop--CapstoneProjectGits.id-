7005f4cff97944374e87a999e5dcdac67d4fdcdb
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-05-29 19:33:18 +0700

    menambahkan produk ke cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,24 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
-    use HasFactory;
-    protected $fillable = [
-        'product_id',
-        'total_price'
-    ];
 
-    public function user()
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
-
-    public function transection()
+    public function product(): BelongsTo
     {
-        return $this->hasMany(Transection::class);
+        return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 }
