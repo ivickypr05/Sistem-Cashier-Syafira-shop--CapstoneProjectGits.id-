c85f2ccfbb4d85a74a7a44094d9f10c698830329
Siti Mutamimah <sitimutamimah669@gmail.com>
2023-06-10 18:26:33 +0700

    add font

--- resources/views/admin/dadmin.blade.php
+++ resources/views/admin/dadmin.blade.php
@@ -2,11 +2,17 @@
 
 @section('title', 'SRC Syafira | Admin Page')
 @section('content')
-    <style>
-        body {
-            background-image: url('/assets/img/bgblur.png');
-        }
-    </style>
+<head>
+    <link href="{{ asset('assets/css/admin.css') }}" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk:wght@600&family=Merriweather+Sans:wght@400;600&display=swap" rel="stylesheet">
+</head>
+<style>
+    body{background-image: url("/assets/img/bgblur.png");}
+    .h1 .p {
+        background-color: rgb(146, 140, 140);
+        font-family: 'Familjen Grotesk', sans-serif;
+    }
+</style>
     <section data-bs-version="5.1" class="header7 cid-sUp808fGej" id="aheader7-2 mt-3">
         <div class="mbr-overlay" style="opacity: 1; background-color: rgb(255, 255, 255);"></div>
 
@@ -14,10 +20,10 @@
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-10 mt-5">
 
-                    <h1 class="mbr-section-title mbr-fonts-style text-light align-center display-1"><strong>Selamat datang
+                    <h1 class="mbr-section-title mbr-fonts-style text-dark align-center display-1"><strong>Selamat datang
                             kembali
                             Admin "SRC Syafira"</strong></h1>
-                    <p class="mbr-text mbr-fonts-style-4 align-center text-light display-5">
+                    <p class="mbr-text mbr-fonts-style-4 align-center text-dark display-5">
                         <br>
                         Silahkan Tambah, Ubah, dan Delete data sesuai yang admin inginkan, Semoga makin banyak Transaksinya
                         >.< </p>
